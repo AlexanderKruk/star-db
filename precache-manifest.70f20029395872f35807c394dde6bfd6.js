6c7c24f6453d81ebf98c4e043378895d4f90776d
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-08-19 10:47:06 +0300

    Updates

---
+++ precache-manifest.70f20029395872f35807c394dde6bfd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0ff441fd2c6e045755bdcf51edf039",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "042aeb048b74385a33a5",
+    "url": "/star-db/static/css/main.174e9483.chunk.css"
+  },
+  {
+    "revision": "c34df46636492aae998f",
+    "url": "/star-db/static/js/2.d0fea851.chunk.js"
+  },
+  {
+    "revision": "042aeb048b74385a33a5",
+    "url": "/star-db/static/js/main.3ab288a3.chunk.js"
+  },
+  {
+    "revision": "7fd0c1ce602d36977d9a",
+    "url": "/star-db/static/js/runtime~main.82ef0156.js"
+  }
+]);
